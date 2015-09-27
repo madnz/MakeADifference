47e3ed971741b6ea38784171f8f0c9de28625a0a
Adrian Parker <adrian@iceknife.com>
2015-09-27 22:40:31 +1300

    Hoist signup complete.

--- deployToProd.sh
+++ deployToProd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'Deploy begun'
-rsync -av --exclude='.git' --exclude='.idea' --exclude='README.md' --exclude='deployToProd.sh' ./ ../madnz.github.io/
+rsync -av --exclude='node_modules' --exclude='.git' --exclude='.idea' --exclude='README.md' --exclude='deployToProd.sh' ./ ../madnz.github.io/
 cd ../madnz.github.io
 git add .
 git commit -m "Update from ./MakeADifference"

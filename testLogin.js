3b0f9068363647ca70a4cc1b589ac7c5fb593e5d
Adrian Parker <adrian@iceknife.com>
2016-05-02 20:24:28 +1200

    Show vacancy list on home page

--- testLogin.js
+++ testLogin.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var userUID, userName, userRole, volunteerTable,
+var userUID, userName, userRole, volunteerTable, projectTable,
     firebase = new Firebase('https://scorching-fire-2466.firebaseIO.com/');
 
 window.onload = function () {
@@ -8,9 +8,11 @@ window.onload = function () {
     document.getElementById('volunteersAdminButton').onclick = volunteers;
     document.getElementById('projectAdminButton').onclick = projects;
 }
+
 function showInWorkArea(html) {
     document.getElementById('workArea').innerHTML = html;
 }
+
 function showWorkAreaLoading() {
     showInWorkArea('<h4>loading...</h4>');
 }
@@ -34,15 +36,63 @@ function volunteers() {
         console.log("The read failed: " + errorObject.code);
     });
 }
+
 function generateVolunteerHTML(volunteer) {
     var volunteerObj = volunteer.val();
     volunteerTable += '<tr><td>' + volunteerObj.name + '</td><td>' + volunteerObj.email + '</td><td>' + volunteerObj.skills + '</td></tr>';
 }
 
+function generateProjectHTML(project) {
+    var projectObj = project.val();
+    projectTable += '<tr><td>' + projectObj.organisationName + '</td><td>' + projectObj.vacancyName + '</td></tr>';
+}
+
 function projects() {
     console.log('projects');
+    projectTable = '';
     showWorkAreaLoading();
     // get list of projects
+    var projectsRef = firebase.child('projects');
+    projectsRef.on("value", function (snapshot) {
+        if (snapshot.hasChildren()) {
+            projectTable = '<table id="projectTable"><thead><tr><th>Organisation</th><th>Vacancy</th></tr></thead><tbody>';
+            snapshot.forEach(generateProjectHTML);
+            projectTable += '</tbody></table>';
+            // add link for exporting to csv
+            projectTable += '<br/><a download="project.csv" href="#" onclick="return ExcellentExport.csv(this, \'projectTable\');">Export to CSV</a>';
+        } else {
+            projectTable = '<h4>No projects available</h4>';
+        }
+        // add Add New link
+        projectTable += '<br/><a href="#" id="projectAddButton" onclick="projectAdd();">Add Project</a>';
+        showInWorkArea(projectTable);
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+}
+
+function createProject() {
+    console.log('create project');
+    document.getElementById('projectForm').style.display = 'none';
+    showWorkAreaLoading();
+    var newProject = {
+        "organisationName": document.getElementById('organisationName').value,
+        "organisationType": document.getElementById('organisationType').value,
+        "vacancyName": document.getElementById('vacancyName').value,
+        "vacancyDates": document.getElementById('vacancyDates').value,
+        "vacancyLocations": document.getElementById('vacancyLocations').value,
+        "vacancySkills": document.getElementById('vacancySkills').value,
+        "vacancyDescription": document.getElementById('vacancyDescription').value
+    };
+    var projectsRef = firebase.child('projects');
+    var newProjectRef = projectsRef.push(newProject);
+    console.log("Saved project with new key: "+newProjectRef.key());
+    projects();
+}
+
+function projectAdd() {
+    console.log('add project');
+    document.getElementById('projectForm').style.display = 'block';
 }
 
 function login() {
